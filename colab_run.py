133c45c03987fb863b1f9cd52344f3ffd8e33853
pingu <giorgiosacchi@me.com>
2022-04-27 09:52:01 +0200

    to initializer

--- colab_run.py
+++ colab_run.py
@@ -1,7 +1,6 @@
 import os
 # comment out below line to enable tensorflow logging outputs
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
-import time
 import tensorflow as tf
 physical_devices = tf.config.experimental.list_physical_devices('GPU')
 if len(physical_devices) > 0:
@@ -9,36 +8,30 @@ if len(physical_devices) > 0:
 from absl import app, flags, logging
 from absl.flags import FLAGS
 import core.utils as utils
-from core.yolov4 import filter_boxes
 from tensorflow.python.saved_model import tag_constants
 from core.config import cfg
-from PIL import Image
-import cv2
-import numpy as np
-import matplotlib.pyplot as plt
 from tensorflow.compat.v1 import ConfigProto
 from tensorflow.compat.v1 import InteractiveSession
 # deep sort imports
 from deep_sort import preprocessing, nn_matching
-from deep_sort.detection import Detection
 from deep_sort.tracker import Tracker
 from tools import generate_detections as gdet
-flags.DEFINE_string('framework', 'tf', '(tf, tflite, trt')
-flags.DEFINE_string('weights', './checkpoints/yolov4-416',
-                    'path to weights file')
-flags.DEFINE_integer('size', 416, 'resize images to')
-flags.DEFINE_boolean('tiny', False, 'yolo or yolo-tiny')
-flags.DEFINE_string('model', 'yolov4', 'yolov3 or yolov4')
-flags.DEFINE_string('video', './data/video/test.mp4', 'path to input video or set to 0 for webcam')
-flags.DEFINE_string('output', None, 'path to output video')
-flags.DEFINE_string('output_format', 'XVID', 'codec used in VideoWriter when saving video to file')
-flags.DEFINE_float('iou', 0.45, 'iou threshold')
-flags.DEFINE_float('score', 0.50, 'score threshold')
-flags.DEFINE_boolean('dont_show', False, 'dont show video output')
-flags.DEFINE_boolean('info', False, 'show detailed info of tracked objects')
-flags.DEFINE_boolean('count', False, 'count objects being tracked on screen')
 
-def main(_argv):
+def initializer(_argv):
+    flags.DEFINE_string('framework', 'tf', '(tf, tflite, trt')
+    flags.DEFINE_string('weights', './checkpoints/yolov4-416', 'path to weights file')
+    flags.DEFINE_integer('size', 416, 'resize images to')
+    flags.DEFINE_boolean('tiny', False, 'yolo or yolo-tiny')
+    flags.DEFINE_string('model', 'yolov4', 'yolov3 or yolov4')
+    flags.DEFINE_string('video', './data/video/test.mp4', 'path to input video or set to 0 for webcam')
+    flags.DEFINE_string('output', None, 'path to output video')
+    flags.DEFINE_string('output_format', 'XVID', 'codec used in VideoWriter when saving video to file')
+    flags.DEFINE_float('iou', 0.45, 'iou threshold')
+    flags.DEFINE_float('score', 0.50, 'score threshold')
+    flags.DEFINE_boolean('dont_show', False, 'dont show video output')
+    flags.DEFINE_boolean('info', False, 'show detailed info of tracked objects')
+    flags.DEFINE_boolean('count', False, 'count objects being tracked on screen')
+
     # Definition of the parameters
     max_cosine_distance = 0.4
     nn_budget = None
@@ -72,9 +65,3 @@ def main(_argv):
         infer = saved_model_loaded.signatures['serving_default']
     
     return tracker, infer, interpreter, encoder, FLAGS
-
-if __name__ == '__main__':
-    try:
-        app.run(main)
-    except SystemExit:
-        pass
