5ebf99dc7937994b8aa6aea30e148447ebe88d3a
Group15 <group15@enaccluster001.intranet.epfl.ch>
2022-05-24 14:56:18 +0200

    Converted loop to class & function

--- object_tracker.py
+++ object_tracker.py
@@ -3,7 +3,7 @@ import os
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 import time
 import tensorflow as tf
-tf.config.optimizer.set_jit(True)
+# tf.config.optimizer.set_jit(True)
 physical_devices = tf.config.experimental.list_physical_devices('GPU')
 if len(physical_devices) > 0:
     tf.config.experimental.set_memory_growth(physical_devices[0], True)
@@ -22,6 +22,7 @@ from deep_sort import nn_matching
 from deep_sort.detection import Detection
 from deep_sort.tracker import Tracker
 from tools import generate_detections as gdet
+from tensorflow.python.keras.models import load_model
 
 
 flags.DEFINE_string('weights', './checkpoints/yolov4-416',
@@ -102,8 +103,8 @@ def main(_argv):
     # while video is running
     while True:
         return_value, frame = vid.read()
-        movenet_frame = frame.copy()
         if return_value:
+            movenet_frame = frame.copy()    
             frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
         else:
             print('Video has ended or failed, try a different video format!')
@@ -247,8 +248,169 @@ def main(_argv):
         if cv2.waitKey(1) & 0xFF == ord('q'): break
     cv2.destroyAllWindows()
 
+
+
+class Object_Tracker():
+
+    def __init__(self):
+        physical_devices = tf.config.experimental.list_physical_devices('GPU')
+        if len(physical_devices) > 0:
+            tf.config.experimental.set_memory_growth(physical_devices[0], True)
+        max_cosine_distance = 0.4
+        nn_budget = None
+        
+        # initialize deep sort
+        model_filename = 'model_data/mars-small128.pb'
+        self.encoder = gdet.create_box_encoder(model_filename, batch_size=1)
+        # calculate cosine distance metric
+        metric = nn_matching.NearestNeighborDistanceMetric("cosine", max_cosine_distance, nn_budget)
+        # initialize tracker
+        self.tracker = Tracker(metric, max_age=500)
+
+        # load configuration for object detector
+        config = ConfigProto()
+        config.gpu_options.allow_growth = True
+        self.input_size = 416
+
+        # saved_model_loaded = tf.saved_model.load('./checkpoints/yolov4-tiny-416', tags=[tag_constants.SERVING])
+        saved_model_loaded = load_model('./checkpoints/yolov4-tiny-416')
+        self.infer = saved_model_loaded.signatures['serving_default']
+
+        # loading movenet model
+        movenet_model = tf.saved_model.load("./checkpoints/movenet_multipose_lightning_1")
+        self.movenet = movenet_model.signatures['serving_default']
+        self.saved_id = None
+
+        # begin video capture
+
+    def image_track(self, image):
+        frame = cv2.cvtColor(np.array(image), cv2.COLOR_RGB2BGR) ## TODO test if we need this conversion
+        frame = cv2.cvtColor(frame,cv2.COLOR_BGR2RGB)
+
+        image_data = cv2.resize(frame, (self.input_size, self.input_size))
+        image_data = image_data / 255.
+        image_data = image_data[np.newaxis, ...].astype(np.float32)
+
+        movenet_frame = tf.image.resize_with_pad(tf.expand_dims(frame, axis=0), 384, 640)
+        movenet_frame = tf.cast(movenet_frame, dtype=tf.int32)
+        movenet_results = self.movenet(movenet_frame)
+
+        bounding_boxes = movenet_results['output_0'].numpy()[:,:,51:56].reshape((6,1,5))
+        keypoints_with_scores = movenet_results['output_0'].numpy()[:,:,:51].reshape((6,17,3))
+        reinit_bboxes = loop_through_people(frame, keypoints_with_scores, 0.45, bounding_boxes)
+
+        batch_data = tf.constant(image_data)
+
+        pred_bbox = self.infer(batch_data)
+
+        for key, value in pred_bbox.items():
+            boxes = value[:, :, 0:4]
+            pred_conf = value[:, :, 4:]
+
+        boxes, scores, classes, valid_detections = tf.image.combined_non_max_suppression(
+            boxes=tf.reshape(boxes, (tf.shape(boxes)[0], -1, 1, 4)),
+            scores=tf.reshape(
+                pred_conf, (tf.shape(pred_conf)[0], -1, tf.shape(pred_conf)[-1])),
+            max_output_size_per_class=50,
+            max_total_size=50,
+            iou_threshold=0.45,
+            score_threshold=0.5
+        )
+
+        # convert data to numpy arrays and slice out unused elements
+        num_objects = valid_detections.numpy()[0]
+        bboxes = boxes.numpy()[0]
+        bboxes = bboxes[0:int(num_objects)]
+        scores = scores.numpy()[0]
+        scores = scores[0:int(num_objects)]
+        classes = classes.numpy()[0]
+        classes = classes[0:int(num_objects)]
+
+        # format bounding boxes from normalized ymin, xmin, ymax, xmax ---> xmin, ymin, width, height
+        original_h, original_w, _ = frame.shape
+        bboxes = utils.format_boxes_x_y_w_h(bboxes, original_h, original_w)
+
+        # store all predictions in one parameter for simplicity when calling functions
+        pred_bbox = [bboxes, scores, classes, num_objects]
+
+        # read in all class names from config
+        class_names = utils.read_class_names(cfg.YOLO.CLASSES)
+
+        # custom allowed classes (uncomment line below to customize tracker for only people)
+        allowed_classes = ['person']
+
+        # loop through objects and use class index to get class name, allow only classes in allowed_classes list
+        names = []
+        deleted_indx = []
+        for i in range(num_objects):
+            class_indx = int(classes[i])
+            class_name = class_names[class_indx]
+            if class_name not in allowed_classes:
+                deleted_indx.append(i)
+            else:
+                names.append(class_name)
+        names = np.array(names)
+
+        # delete detections that are not in allowed_classes
+        bboxes = np.delete(bboxes, deleted_indx, axis=0)
+        scores = np.delete(scores, deleted_indx, axis=0)
+
+        # encode yolo detections and feed to tracker
+        features = self.encoder(frame, bboxes)
+        detections = [Detection(bbox, score, class_name, feature) for bbox, score, class_name, feature in zip(bboxes, scores, names, features)]
+
+        #initialize color map
+        cmap = plt.get_cmap('tab20b')
+        colors = [cmap(i)[:3] for i in np.linspace(0, 1, 20)]
+
+        # run non-maxima supression
+        boxs = np.array([d.tlwh for d in detections])
+        scores = np.array([d.confidence for d in detections])
+        indices = []
+        if len(boxs) > 0:
+            indices = tf.image.non_max_suppression(boxs, scores, 50, iou_threshold=0.45,
+                score_threshold=0.5)
+        detections = [detections[i] for i in indices]
+
+        self.tracker.predict()
+        self.tracker.update(detections)
+
+        if reinit_bboxes is not None:
+            reinit_bboxes = utils.format_boxes_x_y_x_y([reinit_bboxes[0:4]], original_h, original_w)[0]
+            max_iou_track = get_max_iou_track(reinit_bboxes, self.tracker.tracks)
+            self.saved_id = max_iou_track.track_id
+        
+        # update tracks
+        for track in self.tracker.tracks:
+            if not track.is_confirmed(): #or track.track_id != 1: # or track.time_since_update > 1:
+                continue
+            if not track.track_id == self.saved_id:
+                continue
+            bbox = track.to_tlbr()
+            class_name = track.get_class()
+            print("reinit:", reinit_bboxes)
+            print("bbox:", bbox)
+        
+        # draw bbox on screen
+            color = colors[int(track.track_id) % len(colors)]
+            color = [i * 255 for i in color]
+            cv2.rectangle(frame, (int(bbox[0]), int(bbox[1])), (int(bbox[2]), int(bbox[3])), color, 2)
+            cv2.rectangle(frame, (int(bbox[0]), int(bbox[1]-30)), (int(bbox[0])+(len(class_name)+len(str(track.track_id)))*17, int(bbox[1])), color, -1)
+            cv2.putText(frame, class_name + "-" + str(track.track_id),(int(bbox[0]), int(bbox[1]-10)),0, 0.75, (255,255,255),2)
+
+        result = np.asarray(frame)
+        result = cv2.cvtColor(frame, cv2.COLOR_RGB2BGR)
+        cv2.imshow("Output Video", result)
+        
+        return None, [1]
+
 if __name__ == '__main__':
     try:
-        app.run(main)
+        # app.run(main)
+        ob_tracker = Object_Tracker()
+        from PIL import Image
+        with Image.open("./People.png") as im:
+            ob_tracker.image_track(im)
+
     except SystemExit:
         pass
